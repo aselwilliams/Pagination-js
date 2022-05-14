7f13995ce9d4fe43d0e77932d9f072b0014f7344
aselwilliams <aselwilliams22@gmail.com>
2022-05-14 12:10:20 -0400

    structured the table with new data

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ const tableBody = document.querySelector('.info')
 
 fetch('https://jsonplaceholder.typicode.com/comments')
 .then(response => response.json())
-.then(json =>{
-    console.log(json)
+.then(data =>{
+    console.log(data)
+    
    for (let i=0; i<data.length; i++){
        tableBody.innerHTML+=`
-       `
+        <tr>
+            <td>${data[i].id}</td>
+            <td>${data[i].email}</td>
+            <td>${data[i].name}</td>
+            <td>${data[i].postId}</td>
+        </tr>`
    }
 
 })
