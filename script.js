0e7405470420b20e4d9c6ebb4377d19f169ce265
aselwilliams <aselwilliams22@gmail.com>
2022-05-14 12:41:37 -0400

    changes in script

--- script.js
+++ script.js
@@ -1,18 +1,80 @@
-const tableBody = document.querySelector('.info')
+const tableBody = document.querySelector('.info');
+const nextBtn=document.querySelector('.next')
+const prevBtn=document.querySelector('.prev')
+const data=[]
 
-fetch('https://jsonplaceholder.typicode.com/comments')
-.then(response => response.json())
-.then(data =>{
-    console.log(data)
+renderData=()=>{
+    fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(response => response.json())
+    .then(data =>{
+        console.log(data)
+        // const data=data
+       for (let i=0; i<data.length; i++){
+           tableBody.innerHTML+=`
+            <tr>
+                <td>${data[i].id}</td>
+                <td>${data[i].email}</td>
+                <td>${data[i].name}</td>
+                <td>${data[i].postId}</td>
+            </tr>`
+       }
     
-   for (let i=0; i<data.length; i++){
-       tableBody.innerHTML+=`
-        <tr>
-            <td>${data[i].id}</td>
-            <td>${data[i].email}</td>
-            <td>${data[i].name}</td>
-            <td>${data[i].postId}</td>
-        </tr>`
-   }
+    })
+    
+    let pageNumber=0;
+    let limit=20;
+    const lastPage=Math.ceil(data.length/limit)
+    const start=pageNumber*limit;
+    const end= (pageNumber+1) *limit
+    
+    if(pageNumber===0){
+        prevBtn.disabled=true
+    } else {
+        prevBtn.disabled=false
+    }
+    if(pageNumber===lastPage){
+        nextBtn.disabled=true
+    } else {
+        nextBtn.disabled=false;
+    }
+    
+    const partialData= data.slice(start, end)
+    console.log(partialData)
+}
+renderData()
+// fetch('https://jsonplaceholder.typicode.com/comments')
+// .then(response => response.json())
+// .then(data =>{
+//     console.log(data)
+//     // const data=data
+//    for (let i=0; i<data.length; i++){
+//        tableBody.innerHTML+=`
+//         <tr>
+//             <td>${data[i].id}</td>
+//             <td>${data[i].email}</td>
+//             <td>${data[i].name}</td>
+//             <td>${data[i].postId}</td>
+//         </tr>`
+//    }
 
-})
+// })
+
+// let pageNumber=0;
+// let limit=20;
+// const lastPage=Math.ceil(data.length/limit)
+// const start=pageNumber*limit;
+// const end= (pageNumber+1) *limit
+
+// if(pageNumber===0){
+//     prevBtn.disabled=true
+// } else {
+//     prevBtn.disabled=false
+// }
+// if(pageNumber===lastPage){
+//     nextBtn.disabled=true
+// } else {
+//     nextBtn.disabled=false;
+// }
+
+// const partialData= data.slice(start, end)
+// console.log(partialData)
